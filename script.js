e0771aaf43fd36b52281decc25ac8df891707ad7
iso-yuki <134838049+iso-yuki@users.noreply.github.com>
2024-08-29 13:10:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const gameArea = document.getElementById('gameArea');
 // 画像の移動位置を定義
 const positions = [
     { top: '0px', left: '0px' },
-    { top: '0px', left: 'calc(50% - 100px)' },
-    { top: 'calc(50% - 100px)', left: '0px' },
-    { top: 'calc(50% - 100px)', left: 'calc(50% - 100px)' }
+    { top: '0px', left: 'calc(100% - 100px)' },
+    { top: 'calc(100% - 100px)', left: '0px' },
+    { top: 'calc(100% - 100px)', left: 'calc(100% - 100px)' }
 ];
 
 // クリック時に画像を移動させる関数
@@ -24,29 +24,3 @@ function moveImage() {
 
 // 画像がクリックされたときのイベントリスナー
 image.addEventListener('click', moveImage);
-
-// ウィンドウサイズの変更に対応するための関数
-function adjustImageSize() {
-    const gameAreaWidth = gameArea.clientWidth;
-    const gameAreaHeight = gameArea.clientHeight;
-    
-    const img = new Image();
-    img.src = image.src;
-    img.onload = function() {
-        const imgAspect = img.width / img.height;
-        const gameAreaAspect = gameAreaWidth / gameAreaHeight;
-
-        if (imgAspect > gameAreaAspect) {
-            image.style.width = 'auto';
-            image.style.height = '100%';
-        } else {
-            image.style.width = '100%';
-            image.style.height = 'auto';
-        }
-    };
-}
-
-// 初期サイズ調整
-adjustImageSize();
-// ウィンドウのリサイズイベントに対応
-window.addEventListener('resize', adjustImageSize);
